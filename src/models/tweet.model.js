b400e0d65de82328c5965bb0513e3a8fe85b55ba
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-01-28 23:49:09 +0530

    document middlware in mongoose

--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -31,6 +31,16 @@ const tweetSchema = new Schema(
   { timestamps: true }
 );
 
+tweetSchema.pre("save", function (next) {
+  this.content = this.content + "saturday";
+  console.log("pre");
+  next();
+});
+
+tweetSchema.post("save", function () {
+  console.log("post");
+});
+
 const Tweet = mongoose.model("Tweet", tweetSchema);
 
 module.exports = Tweet;
